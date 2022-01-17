8e7d0ce854b800d4bdf5bb1283593fb7b3090ecc
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-17 17:07:36 -0300

    requisito 1: ok

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-// add your implementation here
+const average = (array) => {
+  let soma = 0;
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  media = Math.round(soma / array.length);
+  return media;
+};
+
 module.exports = average;
