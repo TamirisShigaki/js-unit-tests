82dbc3b7dc3b3cf7c1cd7e1672ead93979e0dfdf
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-17 18:04:35 -0300

    requisito 3: ok

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'string' || typeof idade !== 'number') {
+    return undefined;
+  }
+  return (`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
+};
 
 module.exports = vqv;
