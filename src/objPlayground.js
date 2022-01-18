e07876b31c4b76b821a10f8444120eaf834bcfd0
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-17 23:37:34 -0300

    requisito 7: ok

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const calculo = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
+  return calculo;
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  let resultado;
+  if (type === 'keys') {
+    resultado = Object.keys(object); 
+  } else if (type === 'values') {
+    resultado = Object.values(object);
+  } else {
+    resultado = Object.entries(object);
+  }
+  return resultado;
+};
 
 module.exports = { calculator, arrayGenerator };
