d996a10aa468613c8284a20c164a772d95f5f9ec
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-19 00:49:51 -0300

    requisito 4: modificado, finalizado

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -48,11 +48,13 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
 
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
 
-    expect(circle(3).area).toEqual(28.259999999999998);
+    expect(circle(3).area).toBeCloseTo(28.26);
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
 
-    expect(circle(3)).toEqual({radius: 3, area: 28.259999999999998, circumference: 18.84});
+    expect(circle(3).radius).toBeCloseTo(3);
+    expect(circle(3).area).toBeCloseTo(28.26)
+    expect(circle(3).circumference).toBeCloseTo(18.84)
 
   });
 });
